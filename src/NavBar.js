bb818081fe5144f99540fc7540f89b55e8699386
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-06-13 13:31:45 +0200

    Summary items into SummaryItem component

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,22 +1,38 @@
 import { useState } from "react";
 export default function NavBar({ movies }) {
-  const [query, setQuery] = useState("");
   return (
     <nav className="nav-bar">
-      <div className="logo">
-        <span role="img">🍿</span>
-        <h1>usePopcorn</h1>
-      </div>
-      <input
-        className="search"
-        type="text"
-        placeholder="Search movies..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <p className="num-results">
-        Found <strong>{movies.length}</strong> results
-      </p>
+      <Logo />
+      <Search />
+      <NumResults movies={movies} />
     </nav>
+  );
+}
+
+function NumResults({ movies }) {
+  return (
+    <p className="num-results">
+      Found <strong>{movies.length}</strong> results
+    </p>
+  );
+}
+function Logo() {
+  return (
+    <div className="logo">
+      <span role="img">🍿</span>
+      <h1>usePopcorn</h1>
+    </div>
+  );
+}
+function Search() {
+  const [query, setQuery] = useState("");
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+    />
   );
 }
--- src/Summary.js
+++ src/Summary.js
@@ -31,7 +31,6 @@ export default function Summary({ avgs, isOpen }) {
   const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
   const avgUserRating = average(watched.map((movie) => movie.userRating));
   const avgRuntime = average(watched.map((movie) => movie.runtime));
-  const averages = { avgImdbRating, avgUserRating, avgRuntime };
   return (
     <>
       {isOpen && (
@@ -39,22 +38,10 @@ export default function Summary({ avgs, isOpen }) {
           <div className="summary">
             <h2>Movies you watched</h2>
             <div>
-              <p>
-                <span>#️⃣</span>
-                <span>{watched.length} movies</span>
-              </p>
-              <p>
-                <span>⭐️</span>
-                <span>{avgImdbRating}</span>
-              </p>
-              <p>
-                <span>🌟</span>
-                <span>{avgUserRating}</span>
-              </p>
-              <p>
-                <span>⏳</span>
-                <span>{avgRuntime} min</span>
-              </p>
+              <SummaryItem icon={"#️⃣"}>{watched.length} movies</SummaryItem>
+              <SummaryItem icon={"⭐️"}>{avgImdbRating}</SummaryItem>
+              <SummaryItem icon={"🌟"}>{avgUserRating}</SummaryItem>
+              <SummaryItem icon={"⏳"}>{avgRuntime}min</SummaryItem>
             </div>
           </div>
           <WatchedMoviesList watched={watched} />
@@ -63,6 +50,7 @@ export default function Summary({ avgs, isOpen }) {
     </>
   );
 }
+
 function WatchedMoviesList({ watched }) {
   return (
     <ul className="list">
@@ -71,21 +59,21 @@ function WatchedMoviesList({ watched }) {
           <img src={movie.Poster} alt={`${movie.Title} poster`} />
           <h3>{movie.Title}</h3>
           <div>
-            <p>
-              <span>⭐️</span>
-              <span>{movie.imdbRating}</span>
-            </p>
-            <p>
-              <span>🌟</span>
-              <span>{movie.userRating}</span>
-            </p>
-            <p>
-              <span>⏳</span>
-              <span>{movie.runtime} min</span>
-            </p>
+            <SummaryItem icon={"⭐️"}>{movie.imdbRating}</SummaryItem>
+            <SummaryItem icon={"🌟"}>{movie.userRating}</SummaryItem>
+            <SummaryItem icon={"⏳"}>{movie.runtime} min</SummaryItem>
           </div>
         </li>
       ))}
     </ul>
+  );
+}
+
+function SummaryItem({ children, icon }) {
+  return (
+    <p>
+      <span>{icon}</span>
+      <span>{children}</span>
+    </p>
   );
 }
