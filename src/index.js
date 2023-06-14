4d159563d85a0911e4f166b26e6043f5c7051da1
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-06-14 10:25:18 +0200

    Customizable color, size, class, messages and defaultRating

--- src/index.js
+++ src/index.js
@@ -7,8 +7,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={10} />
-    {/* <StarRating maxRating={7} />
-    <StarRating /> */}
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <StarRating maxRating={10} color="red" size={24} className="test" />
   </React.StrictMode>
 );
