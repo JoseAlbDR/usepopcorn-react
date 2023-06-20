2ee6c9f21e7249bd990716681223f427382397c9
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-06-20 17:06:35 +0200

    Changed http protocol to https

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -17,7 +17,7 @@ export function useMovies(query, callback) {
 
       // Fetch data from API
       const response = await fetch(
-        `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${query}`
+        `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${query}`
       );
 
       // Error handling if bad response
